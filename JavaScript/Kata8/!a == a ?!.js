c0b003d0567af5ecef38ea8f668ee9ecb90121b9
Okechukwu Egeruoh <39450490+Chukwu12@users.noreply.github.com>
2024-04-05 11:48:39 +0000

    is a equal to a?

---
+++ JavaScript/Kata8/!a == a ?!.js
@@ -0,0 +1,11 @@
+// You task to pass only this tests :
+
+// a == false
+// !a == false
+// a == !a
+
+
+let a = [];
+console.log(a == false);  // false
+console.log(!a == false); // true
+console.log(a == !a);     // true
